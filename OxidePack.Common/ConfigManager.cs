17c2ec88704c37c6035d2f2be5547ed51bfd82c3
Maxim Bazuev <bazuka5801@mail.ru>
2018-09-12 21:43:29 +0300

    [Common] Add config auto-reloading

--- OxidePack.Common/ConfigManager.cs
+++ OxidePack.Common/ConfigManager.cs
@@ -1,35 +1,134 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Newtonsoft.Json.Linq;
+using SapphireEngine;
 
 namespace OxidePack
 {
-    public static class ConfigManager
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ConfigAttribute : Attribute
     {
-        public static void Load(Type type)
+        
+    }
+    public class ConfigManager : SapphireType
+    {
+        private Type m_ConfigType;
+        private FSWatcher m_ConfigWatcher;
+        
+        public override void OnAwake()
         {
+            m_ConfigType = FindConfigType();
+            Watch();
+            Load();
+        }
+
+        public void Watch()
+        {
+            if (m_ConfigWatcher != null)
+            {
+                m_ConfigWatcher.Close();
+                m_ConfigWatcher = null;
+            }
+            m_ConfigWatcher = new FSWatcher(AppDomain.CurrentDomain.BaseDirectory, "config.json");
+            m_ConfigWatcher.Subscribe(OnConfigChanged);
+        }
+
+        private void OnConfigChanged(string obj)
+        {
+            Load();
+        }
+
+        public void Load()
+        {
+            if (m_ConfigType == null)
+            {
+                return;
+            }
+            
             if (File.Exists("config.json") == false)
             {
                 Write();
                 return;
             }
-            var config = JObject.Parse(File.ReadAllText("config.json"));
-            foreach (var field in type.GetFields(BindingFlags.Static | BindingFlags.Public))
+            
+            List<string> newFiels = new List<string>();
+            JObject config;
+            try
+            {
+                config = JObject.Parse(File.ReadAllText("config.json"));
+            }
+            catch (Exception e)
+            {
+                ConsoleSystem.LogError("[ConfigManager] Invalid config!\nDetails: "+e.Message);
+                return;
+            }
+            foreach (var field in m_ConfigType.GetFields(BindingFlags.Static | BindingFlags.Public))
             {
                 if (config.ContainsKey(field.Name))
-                    field.SetValue(null, config[field.Name]);
+                {
+                    field.SetValue(null, config[field.Name].ToObject(field.FieldType));
+                }
+                else
+                {
+                    newFiels.Add($"{field.FieldType.Name} {field.Name}");
+                }
             }
+            
+            Write();
+            if (newFiels.Count > 0)
+            {
+                ConsoleSystem.LogWarning($"New fields in config:\n{string.Join("\n", newFiels)}");
+            }
+            ConsoleSystem.Log("Config reloaded!");
         }
-        
-        public static void Write(Type type)
+
+        public void Write()
         {
+            if (m_ConfigType == null)
+            {
+                return;
+            }
+            
             JObject config = new JObject();
-            foreach (var field in type.GetFields(BindingFlags.Static | BindingFlags.Public))
+            foreach (var field in m_ConfigType.GetFields(BindingFlags.Static | BindingFlags.Public))
             {
                 config[field.Name] = JToken.FromObject(field.GetValue(null));
             }
+
+            m_ConfigWatcher.Enabled = false;
             File.WriteAllText("config.json", config.ToString());
+            m_ConfigWatcher.Enabled = true;
+        }
+
+        public Type FindConfigType()
+        {
+            Type configType = null;
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    var configA = type.GetCustomAttribute(typeof(ConfigAttribute));
+                    if (configA != null)
+                    {
+                        if (configType != null)
+                        {
+                            ConsoleSystem.LogError($"[ConfigManger] Multiple config found => <{configType.FullName}> <{type.FullName}>");
+                            return configType;
+                        }
+
+                        configType = type;
+                    }
+                }
+            }
+
+            if (configType == null)
+            {
+                ConsoleSystem.LogError("[ConfigManager] Config class not found");
+            }
+
+            return configType;
         }
     }
 }
---
+++ OxidePack.Common/FSWatcher.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Permissions;
+using SapphireEngine;
+using SapphireEngine.Functions;
+
+namespace OxidePack
+{
+    public class FSWatcher
+    {
+        private class QueuedChange
+        {
+            internal WatcherChangeTypes type;
+            internal Timer timer;
+        }
+
+        // The filesystem watcher
+        private FileSystemWatcher watcher;
+
+        // Changes are buffered briefly to avoid duplicate events
+        private Dictionary<string, QueuedChange> changeQueue;
+
+        private Action<string> callback;
+
+        public void Subscribe(Action<string> action) => callback += action;
+
+        public bool Enabled = true;
+        
+        /// <summary>
+        /// Initializes a new instance of the FSWatcher class
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="filter"></param>
+        public FSWatcher(string directory, string filter)
+        {
+            changeQueue = new Dictionary<string, QueuedChange>();
+            LoadWatcher(directory, filter);
+        }
+
+        /// <summary>
+        /// Loads the filesystem watcher
+        /// </summary>
+        /// <param name="directory"></param>
+        /// <param name="filter"></param>
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private void LoadWatcher(string directory, string filter)
+        {
+            // Create the watcher
+            watcher = new FileSystemWatcher(directory, filter)
+            {
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = true,
+                NotifyFilter = NotifyFilters.LastWrite
+            };
+            watcher.Changed += watcher_Changed;
+            watcher.Created += watcher_Changed;
+            watcher.Deleted += watcher_Changed;
+            watcher.Error += watcher_Error;
+            GC.KeepAlive(watcher);
+        }
+
+        /// <summary>
+        /// Close watcher
+        /// </summary>
+        public void Close()
+        {
+            watcher.Dispose();
+        }
+
+        /// <summary>
+        /// Called when the watcher has registered a filesystem change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (Enabled == false)
+            {
+                return;
+            }
+            
+            var watcher = (FileSystemWatcher)sender;
+            var length = e.FullPath.Length - watcher.Path.Length - Path.GetExtension(e.Name).Length - 1;
+            var sub_path = e.FullPath.Substring(watcher.Path.Length + 1, length);
+            QueuedChange change;
+            if (!changeQueue.TryGetValue(sub_path, out change))
+            {
+                change = new QueuedChange();
+                changeQueue[sub_path] = change;
+            }
+            change.timer?.Dispose();
+            change.timer = null;
+            var path = e.FullPath;
+            switch (e.ChangeType)
+            {
+                case WatcherChangeTypes.Changed:
+                    if (change.type != WatcherChangeTypes.Created)
+                        change.type = WatcherChangeTypes.Changed;
+                    break;
+
+                case WatcherChangeTypes.Created:
+                    if (change.type == WatcherChangeTypes.Deleted)
+                        change.type = WatcherChangeTypes.Changed;
+                    else
+                        change.type = WatcherChangeTypes.Created;
+                    break;
+
+                case WatcherChangeTypes.Deleted:
+                    if (change.type == WatcherChangeTypes.Created)
+                    {
+                        changeQueue.Remove(sub_path);
+                        return;
+                    }
+                    change.type = WatcherChangeTypes.Deleted;
+                    break;
+            }
+            Timer.SetTimeout(() =>
+            {
+                change.timer?.Dispose();
+                change.timer = Timer.SetTimeout(() =>
+                {
+                    change.timer = null;
+                    changeQueue.Remove(sub_path);
+                    switch (change.type)
+                    {
+                        case WatcherChangeTypes.Changed:
+                            FireCallback(path);
+                            break;
+
+                        case WatcherChangeTypes.Created:
+                            FireCallback(path);
+                            break;
+                    }
+                },.2f);
+            }, 0.01f);
+        }
+
+        private void FireCallback(string fullpath)
+        {
+            if (IsFileLocked(fullpath))
+            {
+                Timer.SetTimeout(() => FireCallback(fullpath), 0.1f);
+                return;
+            }
+            callback?.Invoke(fullpath);
+        }
+        
+        
+        protected virtual bool IsFileLocked(string fullpath)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = File.Open(fullpath, FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        private void watcher_Error(object sender, ErrorEventArgs e)
+        {
+            ConsoleSystem.LogError("FSWatcher error: {0}", e.GetException());
+        }
+    }
+}
