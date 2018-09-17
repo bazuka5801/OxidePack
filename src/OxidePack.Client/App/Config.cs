182672e05854928f6db340be6ca6e3d8ad8c0b61
Maxim Bazuev <bazuka5801@mail.ru>
2018-09-17 10:19:35 +0300

    [Client] Update config

--- src/OxidePack.Client/App/Config.cs
+++ src/OxidePack.Client/App/Config.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
 namespace OxidePack.Client.App
 {
     [Config]
@@ -6,5 +10,41 @@ namespace OxidePack.Client.App
         public const string Host       = "127.0.0.1";
         public const int    Port       = 10000;
         public const int    BufferSize = 512;
+
+
+        public static string SolutionFile = "...";
+        
+        [ConfigLoadedEvent]
+        private static Action OnConfigLoadedAction;
+
+        #region [CompilerGenerated]
+        public static event Action OnConfigLoaded
+        {
+            add
+            {
+                Action action = Config.OnConfigLoadedAction;
+                Action action2;
+                do
+                {
+                    action2 = action;
+                    Action value2 = (Action)Delegate.Combine(action2, value);
+                    action = Interlocked.CompareExchange(ref OnConfigLoadedAction, value2, action2);
+                }
+                while (action != action2);
+            }
+            remove
+            {
+                Action action = Config.OnConfigLoadedAction;
+                Action action2;
+                do
+                {
+                    action2 = action;
+                    Action value2 = (Action)Delegate.Remove(action2, value);
+                    action = Interlocked.CompareExchange(ref OnConfigLoadedAction, value2, action2);
+                }
+                while (action != action2);
+            }
+        }
+        #endregion
     }
 }
