b16e3a10dd779f49209ce05b98cc37f5f56a08f7
Maxim Bazuev <bazuka5801@mail.ru>
2018-09-17 10:22:55 +0300

    [Client] Add BuyForm

---
+++ src/OxidePack.Client/Forms/BuyFormo.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace OxidePack.Client.Forms
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopyUID = new System.Windows.Forms.Button();
+            this.tbUID = new System.Windows.Forms.TextBox();
+            this.mStatus = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pbStatus = new System.Windows.Forms.ToolStripProgressBar();
+            this.mStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCopyUID
+            // 
+            this.btnCopyUID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopyUID.Enabled = false;
+            this.btnCopyUID.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCopyUID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCopyUID.Location = new System.Drawing.Point(12, 45);
+            this.btnCopyUID.Name = "btnCopyUID";
+            this.btnCopyUID.Size = new System.Drawing.Size(420, 37);
+            this.btnCopyUID.TabIndex = 1;
+            this.btnCopyUID.Text = "COPY UID";
+            this.btnCopyUID.UseVisualStyleBackColor = true;
+            this.btnCopyUID.Click += new System.EventHandler(this.btnCopyUID_Click);
+            // 
+            // tbUID
+            // 
+            this.tbUID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbUID.Enabled = false;
+            this.tbUID.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbUID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.tbUID.Location = new System.Drawing.Point(12, 12);
+            this.tbUID.Name = "tbUID";
+            this.tbUID.ReadOnly = true;
+            this.tbUID.Size = new System.Drawing.Size(420, 22);
+            this.tbUID.TabIndex = 2;
+            // 
+            // mStatus
+            // 
+            this.mStatus.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pbStatus,
+            this.lblStatus});
+            this.mStatus.Location = new System.Drawing.Point(0, 96);
+            this.mStatus.Name = "mStatus";
+            this.mStatus.Size = new System.Drawing.Size(444, 22);
+            this.mStatus.TabIndex = 5;
+            this.mStatus.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(53, 17);
+            this.lblStatus.Text = "Status:";
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(100, 16);
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 118);
+            this.Controls.Add(this.mStatus);
+            this.Controls.Add(this.tbUID);
+            this.Controls.Add(this.btnCopyUID);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = global::OxidePack.Client.Properties.Resources.oxidepack;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AuthForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auth";
+            this.TopMost = true;
+            this.mStatus.ResumeLayout(false);
+            this.mStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCopyUID;
+        private System.Windows.Forms.TextBox tbUID;
+        private System.Windows.Forms.StatusStrip mStatus;
+        private System.Windows.Forms.ToolStripProgressBar pbStatus;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+    }
+}
---
+++ src/OxidePack.Client/Forms/BuyFormo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxidePack.Client.App;
+
+namespace OxidePack.Client.Forms
+{
+    public partial class AuthForm : Form
+    {
+        public static AuthForm Instance;
+        
+        public AuthForm()
+        {
+            Instance = this;
+            InitializeComponent();
+            UpdateStatus("(1/3) Retrieving MachineID");
+            UpdateProgressBar(style: ProgressBarStyle.Marquee);
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                UpdateKey(MachineIdentificator.Value());
+                UpdateStatus("(2/3) Connecting to server...");
+                UpdateProgressBar(value: 2, max: 3);
+                AppCore.ConnectToServer();
+            });
+        }
+
+        private static void UpdateKey(string key)
+        {
+            RunInMainThread(() =>
+            {
+                Instance.tbUID.Text = key;
+                Instance.btnCopyUID.Enabled = string.IsNullOrEmpty(key) == false;
+            });
+        }
+
+        public static void UpdateStatus(string text)
+        {
+            RunInMainThread(() =>
+            {
+                Instance.lblStatus.Text = $"Status: {text}";
+            });
+        }
+
+        public static void UpdateProgressBar(int value = 0, int max = 100, ProgressBarStyle style = ProgressBarStyle.Blocks)
+        {
+            RunInMainThread(() =>
+            {
+                Instance.pbStatus.Style = style;
+                Instance.pbStatus.Maximum = max;
+                Instance.pbStatus.Value = value;
+            });
+        }
+
+        public static void RunInMainThread(Action action)
+        {
+            if (Instance.InvokeRequired)
+            {
+                Instance.Invoke(action);
+                return;
+            }
+
+            action();
+        }
+
+        private void btnCopyUID_Click( object sender, EventArgs e )
+        {
+            Clipboard.SetText(tbUID.Text);
+        }
+    }
+}
