1ac2f3aa59d063b74b641c99f36618c8eb2d3a73
Maxim Bazuev <bazuka5801@mail.ru>
2018-11-23 21:18:04 +0300

    [Common] Add FileSystemUtils with AdjustFileSize method

---
+++ src/OxidePack.Common/Utils/FileSystemUtils.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace OxidePack.Utils
+{
+    public static class FileSystemUtils
+    {
+        public static (double, string) AdjustFileSize(long fileSizeInBytes)
+        {
+            var names = new[] {"B", "KB", "MB", "GB", "TB"};
+
+            double sizeResult = fileSizeInBytes;
+            int nameIndex = 0;
+            while (sizeResult > 1024 && nameIndex < names.Length)
+            {
+                sizeResult /= 1024; 
+                nameIndex++;
+            }
+
+            if (nameIndex > names.Length - 1)
+            {
+                string lastName = names[nameIndex - 1];
+                throw new OverflowException($"AdjustFileSize: fileSizeInBytes >= 1024{lastName}");
+            }
+
+            return (sizeResult, names[nameIndex]);
+        }
+    }
+}
