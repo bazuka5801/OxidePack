791c8b84955dedc7608fa6b10af8718666c90004
Maxim Bazuev <bazuka5801@mail.ru>
2018-11-25 15:19:27 +0300

    [Client] Add PluginManager form

---
+++ src/OxidePack.Client/Forms/PluginManager/PluginManagerForm.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace OxidePack.Client
+{
+    partial class PluginManagerForm
+    {
+        private System.Windows.Forms.ListBox lbPlugins;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnAddPlugin;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel pnlPlugin;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label lblPluginName;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlModules;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel pnlModulesContainer;
+        private System.Windows.Forms.Button btnDeletePlugin;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPlugins = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAddPlugin = new System.Windows.Forms.Button();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlPlugin = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblPluginName = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlModules = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pnlModulesContainer = new System.Windows.Forms.Panel();
+            this.btnDeletePlugin = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.pnlPlugin.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.pnlModules.SuspendLayout();
+            this.tableLayoutPanel5.SuspendLayout();
+            this.pnlModulesContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbPlugins
+            // 
+            this.lbPlugins.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbPlugins.FormattingEnabled = true;
+            this.lbPlugins.ItemHeight = 18;
+            this.lbPlugins.Location = new System.Drawing.Point(5, 4);
+            this.lbPlugins.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.lbPlugins.Name = "lbPlugins";
+            this.lbPlugins.Size = new System.Drawing.Size(323, 440);
+            this.lbPlugins.TabIndex = 0;
+            this.lbPlugins.SelectedValueChanged += new System.EventHandler(this.lbPlugins_SelectedValueChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAddPlugin, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lbPlugins, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(333, 503);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnAddPlugin
+            // 
+            this.btnAddPlugin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAddPlugin.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAddPlugin.Location = new System.Drawing.Point(5, 452);
+            this.btnAddPlugin.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnAddPlugin.Name = "btnAddPlugin";
+            this.btnAddPlugin.Size = new System.Drawing.Size(323, 47);
+            this.btnAddPlugin.TabIndex = 0;
+            this.btnAddPlugin.Text = "Add";
+            this.btnAddPlugin.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.pnlPlugin, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pnlModules, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(333, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(362, 503);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // pnlPlugin
+            // 
+            this.pnlPlugin.Controls.Add(this.tableLayoutPanel3);
+            this.pnlPlugin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPlugin.Location = new System.Drawing.Point(3, 3);
+            this.pnlPlugin.Name = "pnlPlugin";
+            this.pnlPlugin.Size = new System.Drawing.Size(356, 94);
+            this.pnlPlugin.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Controls.Add(this.lblPluginName, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(356, 94);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // lblPluginName
+            // 
+            this.lblPluginName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblPluginName.Font = new System.Drawing.Font("Verdana", 16F);
+            this.lblPluginName.Location = new System.Drawing.Point(3, 0);
+            this.lblPluginName.Name = "lblPluginName";
+            this.lblPluginName.Size = new System.Drawing.Size(350, 40);
+            this.lblPluginName.TabIndex = 0;
+            this.lblPluginName.Text = "...";
+            this.lblPluginName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 2;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.lblAuthor, 1, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.lblVersion, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 43);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(350, 48);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblAuthor.Location = new System.Drawing.Point(178, 24);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(169, 24);
+            this.lblAuthor.TabIndex = 4;
+            this.lblAuthor.Text = "...";
+            this.lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Author";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblVersion.Location = new System.Drawing.Point(178, 0);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(169, 24);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "0.0.0";
+            this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Version";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlModules
+            // 
+            this.pnlModules.Controls.Add(this.tableLayoutPanel5);
+            this.pnlModules.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlModules.Location = new System.Drawing.Point(3, 103);
+            this.pnlModules.Name = "pnlModules";
+            this.pnlModules.Size = new System.Drawing.Size(356, 397);
+            this.pnlModules.TabIndex = 1;
+            // 
+            // tableLayoutPanel5
+            // 
+            this.tableLayoutPanel5.ColumnCount = 1;
+            this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel5.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel5.Controls.Add(this.pnlModulesContainer, 0, 1);
+            this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+            this.tableLayoutPanel5.RowCount = 2;
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(356, 397);
+            this.tableLayoutPanel5.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Verdana", 14F);
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(350, 40);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Modules";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlModulesContainer
+            // 
+            this.pnlModulesContainer.AutoScroll = true;
+            this.pnlModulesContainer.Controls.Add(this.btnDeletePlugin);
+            this.pnlModulesContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlModulesContainer.Location = new System.Drawing.Point(3, 43);
+            this.pnlModulesContainer.Name = "pnlModulesContainer";
+            this.pnlModulesContainer.Size = new System.Drawing.Size(350, 351);
+            this.pnlModulesContainer.TabIndex = 1;
+            // 
+            // btnDeletePlugin
+            // 
+            this.btnDeletePlugin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnDeletePlugin.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDeletePlugin.ForeColor = System.Drawing.Color.Silver;
+            this.btnDeletePlugin.Location = new System.Drawing.Point(196, 306);
+            this.btnDeletePlugin.Name = "btnDeletePlugin";
+            this.btnDeletePlugin.Size = new System.Drawing.Size(148, 39);
+            this.btnDeletePlugin.TabIndex = 0;
+            this.btnDeletePlugin.Text = "Delete Plugin";
+            this.btnDeletePlugin.UseVisualStyleBackColor = false;
+            this.btnDeletePlugin.Click += new System.EventHandler(this.btnDeletePlugin_Click);
+            // 
+            // PluginManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 503);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = global::OxidePack.Client.Properties.Resources.oxidepack;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.Name = "PluginManagerForm";
+            this.Text = "PluginManagerForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.pnlPlugin.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.pnlModules.ResumeLayout(false);
+            this.tableLayoutPanel5.ResumeLayout(false);
+            this.pnlModulesContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/OxidePack.Client/Forms/PluginManager/PluginManagerForm.cs
@@ -0,0 +1,213 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using OxidePack.Client.Properties;
+using SapphireEngine;
+
+namespace OxidePack.Client
+{
+    public partial class PluginManagerForm : Form
+    {
+        private PluginsProject _PluginsProject;
+        
+        public PluginManagerForm(PluginsProject pluginsProject)
+        {
+            InitializeComponent();
+            LoadProject(pluginsProject);
+        }
+
+        class ModuleListViewItem
+        {
+            public string Name;
+            public string Version;
+
+            public ModuleListViewItem(string name, string version)
+            {
+                this.Name = name;
+                this.Version = version;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name} ({Version})";
+            }
+        }
+
+        void LoadProject(PluginsProject pluginsProject)
+        {
+            this._PluginsProject = pluginsProject;
+            lbPlugins.Items.Clear();
+
+            bool exist = false;
+            pluginsProject.GetPluginList().ForEach(name =>
+            {
+                exist = true;
+                var plugin = pluginsProject.GetPlugin(name);
+                lbPlugins.Items.Add(new ModuleListViewItem(plugin.config.Name, plugin.config.Version.ToString()));
+            });
+
+            if (exist)
+            {
+                // Auto-Select first item
+                lbPlugins.SetSelected(0, true);
+            }
+        }
+
+        class ModuleView
+        {
+            private const int HEIGHT = 52;
+
+            private Panel pnlModule;
+            private Label lblName;
+            private Button btnInfo, btnAddRemove;
+
+            public ModuleView(Panel pnlModule, Label lblName, Button btnInfo, Button btnAddRemove)
+            {
+                this.pnlModule = pnlModule;
+                this.lblName = lblName;
+                this.btnInfo = btnInfo;
+                this.btnAddRemove = btnAddRemove;
+            }
+
+            public void SetIndex(int index)
+            {
+                this.pnlModule.Location = new Point(0, HEIGHT * index);
+            }
+
+            public void SetModuleName(string name)
+            {
+                this.lblName.Text = name;
+            }
+
+            public void SetImageAddRemoveButton(bool enabled)
+            {
+                this.btnAddRemove.BackgroundImage = enabled ? Resources.btnRemove : Resources.btnAdd;
+            }
+        }
+
+        #region [Method] GenerateModuleView
+        private ModuleView GenerateModuleView()
+        {
+            var pnlModule = new Panel
+            {
+                BackgroundImageLayout = ImageLayout.Center,
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(0, 0),
+                Name = $"pnlModule",
+                Size = new Size(pnlModulesContainer.Width, 52),
+                Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right,
+                TabIndex = 0
+            };
+            
+            var tlpModule = new TableLayoutPanel
+            {
+                ColumnCount = 2,
+                Dock = DockStyle.Fill,
+                Location = new Point(0, 0),
+                Name = "tlpModule",
+                RowCount = 1,
+                Size = new Size(437, 50),
+                TabIndex = 0
+            };
+            tlpModule.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpModule.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 120F));
+            tlpModule.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            
+            
+            var lblModuleName = new Label
+            {
+                Dock = DockStyle.Fill,
+                Location = new Point(3, 0),
+                Name = "lblModuleName",
+                Size = new Size(311, 50),
+                TabIndex = 0,
+                TextAlign = ContentAlignment.MiddleLeft
+            };
+            
+            var tlpModuleButtons = new TableLayoutPanel
+            {
+                ColumnCount = 2,
+                Dock = DockStyle.Fill,
+                Location = new Point(320, 3),
+                Name = "tableLayoutPanel6",
+                RowCount = 1,
+                Size = new Size(114, 44),
+                TabIndex = 1
+            };
+            tlpModuleButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpModuleButtons.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 50F));
+            tlpModuleButtons.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            
+            var btnModuleInfo = new Button
+            {
+                BackgroundImage = Resources.info,
+                BackgroundImageLayout = ImageLayout.Zoom,
+                Dock = DockStyle.Fill,
+                ForeColor = Color.White,
+                Location = new Point(3, 3),
+                Name = "btnModuleInfo",
+                Size = new Size(58, 38),
+                TabIndex = 0,
+                UseVisualStyleBackColor = true
+            };
+            
+            var btnAddRemove = new Button
+            {
+                BackColor = Color.Transparent,
+                BackgroundImage = Resources.btnAdd,
+                BackgroundImageLayout = ImageLayout.Zoom,
+                Dock = DockStyle.Fill,
+                Location = new Point(67, 3),
+                Name = "btnAddRemove",
+                Size = new Size(44, 38),
+                TabIndex = 1,
+                UseVisualStyleBackColor = false
+            };
+            
+            pnlModule.Controls.Add(tlpModule);
+            tlpModule.Controls.Add(lblModuleName, 0, 0);
+            tlpModule.Controls.Add(tlpModuleButtons, 1, 0);
+            tlpModuleButtons.Controls.Add(btnModuleInfo, 0, 0);
+            tlpModuleButtons.Controls.Add(btnAddRemove, 1, 0);
+            pnlModulesContainer.Controls.Add(pnlModule);
+            
+            return new ModuleView(pnlModule, lblModuleName, btnModuleInfo, btnAddRemove);
+        }
+        #endregion
+
+        #region [Method] UI Handlers
+
+        #region btnDeletePlugin_Click
+        private void btnDeletePlugin_Click(object sender, System.EventArgs e)
+        {
+            var selected = (ModuleListViewItem)lbPlugins.SelectedItem;
+            var name = selected.Name;
+            if (MessageBox.Show(this, "Are you sure?", $"Deleting {name}...", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2)
+                == DialogResult.Yes)
+            {
+                _PluginsProject.RemovePlugin(name);
+                lbPlugins.Items.Remove(selected);
+            }
+
+        }
+        #endregion
+
+        #region lbPlugins_SelectedValueChanged
+        private void lbPlugins_SelectedValueChanged(object sender, System.EventArgs e)
+        {
+            if (!(lbPlugins.SelectedItem is ModuleListViewItem selected))
+            {
+                return;
+            }
+            
+            var plugin = _PluginsProject.GetPlugin(selected.Name);
+
+            lblPluginName.Text = plugin.config.Name;
+            lblVersion.Text = plugin.config.Version.ToString();
+            lblAuthor.Text = plugin.config.Author;
+        }
+        #endregion
+
+        #endregion
+    }
+}
