f410192428a0370930745edb3d9a9414ac98a73d
Maxim Bazuev <bazuka5801@mail.ru>
2019-01-07 18:02:00 +0300

    [Client] Add root error handling to Initialization method

--- src/OxidePack.Client/Program.cs
+++ src/OxidePack.Client/Program.cs
@@ -9,9 +9,28 @@ using System.Windows.Forms;
 using OxidePack.Client.App;
 using SapphireEngine;
 using SapphireEngine.Functions;
+class Bootstrap
+{
+    static int Initialization(string arg)
+    {
+        try
+        {
+        Program.Main(new string[0]);
 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            File.AppendAllText("error.log", e.Message+"\n"+e.StackTrace+"\n");
+            throw;
+        }
+
+        return -1;
+    }
+}
 namespace OxidePack.Client
 {
+    
     internal class Program
     {
         public static void Main(string[] args)
@@ -22,12 +41,12 @@ namespace OxidePack.Client
 
         static bool KillIfStarted()
         {
-            if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location))
-                    .Count() > 1)
-            {
-                Process.GetCurrentProcess().Kill();
-                return true;
-            }
+//            if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location))
+//                    .Count() > 1)
+//            {
+//                Process.GetCurrentProcess().Kill();
+//                return true;
+//            }
 
             return false;
         }
