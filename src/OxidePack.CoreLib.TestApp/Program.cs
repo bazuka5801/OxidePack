6c82ab46e33c38afc36e2725534bf38de380bbac
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-04 22:01:43 +0300

    Create OxidePack.CoreLib.TestApp project

---
+++ src/OxidePack.CoreLib.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OxidePack.CoreLib.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
