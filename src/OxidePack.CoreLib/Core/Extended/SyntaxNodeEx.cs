c9410ff7c15d6fd2732975e38c0419576493dbed
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-06 16:05:44 +0300

    [CoreLib] Add some extensions methods

--- src/OxidePack.CoreLib/Core/Extended/SyntaxNodeEx.cs
+++ src/OxidePack.CoreLib/Core/Extended/SyntaxNodeEx.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace OxidePack.CoreLib
 {
@@ -18,6 +21,64 @@ namespace OxidePack.CoreLib
                 one = one.Parent;
                 two = two.Parent;
             }
+        }
+        
+        public static string FullPath(this SyntaxNode node)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                sb.Append(node.ToString());
+                while (node.Parent != null)
+                {
+                    switch (node)
+                    {
+                        case MethodDeclarationSyntax m:
+                            sb.Insert(0, $"{m.Identifier.Text}.");
+                            break;
+                        case ClassDeclarationSyntax c:
+                            sb.Insert(0, $"{c.Identifier.Text}.");
+                            break;
+                        case NamespaceDeclarationSyntax n:
+                            sb.Insert(0, $"{n.Name}.");
+                            break;
+                    }
+
+                    node = node.Parent;
+                }
+
+                return sb.ToString();
+            }
+            finally
+            {
+                sb.Clear();
+            }
+        }
+        
+        public static T GetParent<T>(this SyntaxNode node)
+            where T : SyntaxNode
+        {
+            while (node.Parent != null)
+            {
+                node = node.Parent;
+                if (node is T t) return t;
+            }
+
+            return default;
+        }
+        
+        public static string GetFullParameter(this ParameterSyntax param)
+        {
+            string name = "";
+            foreach (var modifier in param.Modifiers)
+            {
+                if (modifier.IsKind(SyntaxKind.RefKeyword))
+                    name += "ref ";
+                else if (modifier.IsKind(SyntaxKind.OutKeyword))
+                    name += "out ";
+            }
+
+            return name += param.Identifier.ToString();
         }
     }
 }
