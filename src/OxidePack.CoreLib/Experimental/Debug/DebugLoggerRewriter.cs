eed86883c9b798409d63a0cf02cd4854efd3a2d6
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-08 02:21:06 +0300

    [CoreLib] Add Debug Logger

---
+++ src/OxidePack.CoreLib/Experimental/Debug/DebugLoggerRewriter.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+namespace OxidePack.CoreLib.Experimental.Debug
+{
+    public class DebugLoggerRewriter : CSharpSyntaxRewriter
+    {
+        private static AdhocWorkspace _workspace = new AdhocWorkspace();
+        
+        private int i = 0;
+        private string indedt = "";
+        
+        public static string Process(string source)
+        {
+            ParseCompilationUnit(source);
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(source);
+            CompilationUnitSyntax root = tree?.GetCompilationUnitRoot();
+            var compilation =  CSharpCompilation.Create("CoreLib")
+                .AddReferences(Directory.GetFiles($"references/")
+                    .Select(path =>
+                        MetadataReference.CreateFromFile(Path.Combine(Directory.GetCurrentDirectory(), path)))
+                    .ToList())
+                .AddSyntaxTrees(tree);
+            
+            root = (CompilationUnitSyntax)new DebugLoggerRewriter().Visit(root);
+            
+            root = root.NormalizeWhitespace();
+            _workspace.Options.WithChangedOption (CSharpFormattingOptions.IndentBraces, true);
+            var formattedCode = Formatter.Format (root, _workspace);
+            return formattedCode.ToFullString();
+            
+        }
+        
+        public override SyntaxNode VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            node = (MethodDeclarationSyntax) base.VisitMethodDeclaration(node);
+            var statements = node.Body.Statements.ToList();
+            statements.Insert(0,SyntaxFactory.ParseStatement($"global::Oxide.Core.Interface.Oxide.LogInfo(\"{indedt}{node.Identifier.Text}\");"));
+            return (node).WithBody(node.Body.WithStatements(SyntaxFactory.List(statements)));
+        }
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            indedt += " ";
+            return base.VisitClassDeclaration(node);
+        }
+    }
+}
