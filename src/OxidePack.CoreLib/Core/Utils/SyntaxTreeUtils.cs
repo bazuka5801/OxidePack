98aefafdb31a8b3f7233f7d0c183b78fe35bc87e
Maxim Bazuev <bazuka5801@mail.ru>
2019-03-09 11:37:31 +0300

    [CoreLib] Add some utils

---
+++ src/OxidePack.CoreLib/Core/Utils/SyntaxTreeUtils.cs
@@ -0,0 +1,46 @@
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace OxidePack.CoreLib.Utils
+{
+    using static SyntaxKind;
+    using static SyntaxFactory;
+    
+    public static class SyntaxTreeUtils
+    {
+        private static readonly AdhocWorkspace Workspace = new AdhocWorkspace();
+
+        public static (CSharpCompilation compilation, CompilationUnitSyntax root) ParseSource(string source, string referencesPath = "references/")
+        {
+            ParseCompilationUnit(source);
+            var tree = CSharpSyntaxTree.ParseText(source);
+            var root = tree?.GetCompilationUnitRoot();
+            var compilation = CSharpCompilation.Create("CoreLib")
+                .AddReferences(Directory.GetFiles(referencesPath)
+                    .Select(path =>
+                        MetadataReference.CreateFromFile(Path.Combine(Directory.GetCurrentDirectory(), path)))
+                    .ToList())
+                .AddSyntaxTrees(tree);
+            
+            return (compilation, root);
+        }
+
+        public static SemanticModel GetSemanticModel(CSharpCompilation compilation, int syntaxTreeIndex = 0)
+        {
+            return compilation.GetSemanticModel(compilation.SyntaxTrees[syntaxTreeIndex]);
+        }
+
+        public static string ConvertToSourceText(CompilationUnitSyntax root)
+        {
+            root = root.NormalizeWhitespace();
+            Workspace.Options.WithChangedOption(CSharpFormattingOptions.IndentBraces, true);
+            var formattedCode = Formatter.Format(root, Workspace);
+            return formattedCode.ToFullString();
+        }
+    }
+}
