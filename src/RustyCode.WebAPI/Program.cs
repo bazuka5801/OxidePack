65fff5c48aae92c762635a44eb491ef8f4b1b25b
Maxim Bazuev <bazuka5801@mail.ru>
2019-04-07 03:40:54 +0300

    Create RustyCode.WebAPI project

---
+++ src/RustyCode.WebAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RustyCode.WebAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
